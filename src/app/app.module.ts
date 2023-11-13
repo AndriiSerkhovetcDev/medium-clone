71a7fcc6d609ed5a5a739219c5706458647fb920
Andrii Serkhovetc <andrii.s@datarails.com>
2023-11-13 18:54:19 +0200

    divided custom modules from root modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,18 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
 
-const rootModules = [
-  BrowserModule,
-  AppRoutingModule,
-  AuthModule,
-  HttpClientModule,
-];
+const rootModules = [BrowserModule, AppRoutingModule, HttpClientModule];
 
 const storeModules = [
   StoreModule.forRoot({}, {}),
   EffectsModule.forRoot([]),
   StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
 ];
+
+const customModules = [AuthModule];
 @NgModule({
   declarations: [AppComponent],
-  imports: [...rootModules, ...storeModules],
+  imports: [...rootModules, ...storeModules, ...customModules],
   providers: [],
   bootstrap: [AppComponent],
 })
